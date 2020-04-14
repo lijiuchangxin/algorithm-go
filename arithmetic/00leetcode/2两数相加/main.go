7fb7a5a1c143608b93507636bc83e47de9a0cc27
李志双 <ljcx@lizhishuangdeMacBook-Pro.local>
2020-04-14 23:49:48 +0800

    update 02

--- arithmetic/00leetcode/2两数相加/main.go
+++ arithmetic/00leetcode/2两数相加/main.go
@@ -20,8 +20,8 @@ type ListNode struct {
 	Next *ListNode
 }
 
-func (this *ListNode) RetNumCount() (res float64) {
-	LinkLen  := this.CountLinkLen()
+func RetNumCount(this *ListNode) (res float64) {
+	LinkLen  := CountLinkLen(this)
 	var val float64
 	temp := this
 	for i := LinkLen; i > 0; i-- {
@@ -33,7 +33,7 @@ func (this *ListNode) RetNumCount() (res float64) {
 	return res
 }
 
-func (this *ListNode) CountLinkLen() (res float64) {
+func CountLinkLen(this *ListNode) (res float64) {
 	temp := this
 	for {
 		if temp.Next == nil { break }
@@ -44,14 +44,13 @@ func (this *ListNode) CountLinkLen() (res float64) {
 }
 
 func RetTwoLinkLen(l1 *ListNode, l2 *ListNode) (len1, len2 float64) {
-	len1, len2 = l1.CountLinkLen(), l2.CountLinkLen()
+	len1, len2 = CountLinkLen(l1), CountLinkLen(l2)
 	return len1, len2
 }
 
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	res := l1.RetNumCount() + l2.RetNumCount()
-	fmt.Println(res)
+	res := RetNumCount(l1) + RetNumCount(l2)
 	len1, len2 := RetTwoLinkLen(l1, l2)
 
 	var len0 float64
@@ -63,10 +62,13 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 
 	head := &ListNode{}
 	temp := head
-	for i := len0-1; i > 0; i-- {
+
+	for i := 0.0; i < len0; i++ {
 		newNode := &ListNode{}
-		val := float64(int(res/math.Pow(10, i-1))%10)
+		val := float64(int(res/math.Pow(10, i))%10)
+		if val == 0 && i == len0 -1 { break }
 		temp.Next = newNode
+		//if i <= len0-1 { temp.Next = newNode }
 		temp.Val = int(val)
 		fmt.Println(temp.Val)
 		temp = newNode
@@ -74,9 +76,21 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	return head
 }
 
+func ShowLink(li *ListNode)  {
+	temp := li
+	for {
+		if temp.Next == nil {
+			break
+		}
+		fmt.Printf("%d =>", temp.Val)
+		temp = temp.Next
+	}
+	fmt.Println()
+}
+
 func main() {
-	// 243
-	// 564
+	//243
+	//564
 	n10 := &ListNode{
 		Val:  2,
 		Next: nil,
@@ -108,7 +122,15 @@ func main() {
 	}
 	n20.Next = n21
 	n21.Next = n22
+	n := addTwoNumbers(n10, n20)
+	ShowLink(n)
 
 
-	fmt.Println(addTwoNumbers(n10, n20))
+
+	//5
+	//5
+	a := &ListNode{5, nil}
+	b := &ListNode{5, nil}
+	ShowLink(addTwoNumbers(a, b))
+
 }
